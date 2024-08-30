99ac014561465734bbab80086f99cdc101487459
nmalviya-vmware <36602512+nmalviya-vmware@users.noreply.github.com>
2024-08-31 02:59:33 +0530

    testing with jenkins

---
+++ boxprint.py
@@ -0,0 +1,12 @@
+def boxprint(symbol, width, height):
+    if len(symbol)!=1:
+        raise Exception('"Symbol" needs to be a string of length 1')
+    if width < 2 or height < 2:
+        raise Exception('"Width" and "Height" should be greater or equal to 2')
+    print(symbol*width)
+    for i in range(height-2):
+        print(symbol+' ' *(width-2)+symbol)
+    print(symbol * width)
+
+#boxprint("**", 16, height=6)
+boxprint(symbol="o",width=4,height=4)
